ec2e139a63ccb71b171066627ace2f73fb873c37
Inc <35619461+vials@users.noreply.github.com>
2021-03-19 17:50:42 -0500

    Update PSN.py

--- PSN.py
+++ PSN.py
@@ -1,15 +1,10 @@
 import requests
 
 username = input("Enter Online ID: ")
-try:
-	response = requests.post('https://accounts.api.playstation.com/api/v1/accounts/onlineIds', json={"onlineId": username, "reserveIfAvailable": False})
-except Exception as e:
-	print(str(e))
-	pass
+response = requests.post('https://accounts.api.playstation.com/api/v1/accounts/onlineIds', json={"onlineId": username, "reserveIfAvailable": False})
 if response.status_code == 400:
-	print("Username: ["+username+"] Taken")
+	print("{} is taken.".format(username))
 elif response.status_code == 201:
-	print("Username: ["+username+"] Available")
+	print("{} is available.".format(username))
 else:
-	print(response.status_code)
-	print(response.content)
+	print("[{}] - {}".format(response.status_code, response.text))
